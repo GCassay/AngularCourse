a3cd79a5591114462b3ee51a70b866f593605bf2
Gea Cassinello <geacassi@publicisgroupe.net>
2017-11-21 10:07:52 -0500

    ngStyle

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,22 +7,41 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <h3>SERVERS COMPONENT</h3>
     <label>Server Name</label>
+
+    <!-- $event -->
     <input type="text"
             (input)="onUptadteServerName($event)"
             placeholder="input event listener ver.">
+
+    <!-- ngModel -->
     <input type="text"
             [(ngModel)]="serverName"
             placeholder="ngModel ver.">
+
+    <!-- Attributes & Click Event -->
     <button [disabled]="!allowNewServer"
             (click)="onCreateServer()">
       Add Server
     </button>
+
+    <!-- String -->
     <p><i>{{ serverName }}</i> {{ serverCreationStatus }}</p>
+
+    <!-- innerText -->
     <p [innerText]="allowNewServer"></p>
+
+    <!-- components -->
     <app-server></app-server>
     <app-server></app-server>
     <app-server></app-server>
-    <p *ngIf="serverCreated">Server Creation Completed</p>`,
+
+    <!-- ngIf Structural directive -->
+    <p *ngIf="serverCreated; else noServer">Server Creation Completed</p>
+
+    <!-- ng-template -->
+    <ng-template #noServer>
+      <p>No new server created</p>
+    </ng-template>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
